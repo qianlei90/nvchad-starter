b26bd4b6a1b332dc5affa8c07aa514a962c5b3ec
qianlei.qianl <qianlei.qianl@bytedance.com>
2024-12-11 18:07:58 +0800

    fix config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,26 +1,26 @@
 local configs = require "nvchad.configs.lspconfig"
 
 local servers = {
-  gopls = {},
-  golangci_lint_ls = {},
-  pylsp = {},
-  lua_ls = {
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = {
-            "vim",
-          }
+    gopls = {},
+    golangci_lint_ls = {},
+    pylsp = {},
+    lua_ls = {
+        settings = {
+            Lua = {
+                diagnostics = {
+                    globals = {
+                        "vim",
+                    }
+                }
+            }
         }
-      }
-    }
-  },
+    },
 }
 
 for name, opts in pairs(servers) do
-  opts.on_init = configs.on_init
-  opts.on_attach = configs.on_attach
-  opts.capabilities = configs.capabilities
+    opts.on_init = configs.on_init
+    opts.on_attach = configs.on_attach
+    opts.capabilities = configs.capabilities
 
-  require("lspconfig")[name].setup(opts)
+    require("lspconfig")[name].setup(opts)
 end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,24 +1,34 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-  {
-    "justinmk/vim-sneak",
-    lazy = false,
-    config = function()
-      -- 启用 label 功能
-      vim.g['sneak#label'] = 1
-    end,
-  },
-  {
-    "tpope/vim-repeat",
-    lazy = false
-  },
-  {
-    "tpope/vim-surround",
-    lazy = false
-  }
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
+    {
+        "justinmk/vim-sneak",
+        lazy = false,
+        config = function()
+            -- 启用 label 功能
+            vim.g['sneak#label'] = 1
+        end,
+    },
+    {
+        "tpope/vim-repeat",
+        lazy = false
+    },
+    {
+        "tpope/vim-surround",
+        lazy = false
+    },
+    {
+        "f-person/git-blame.nvim",
+        event = "VeryLazy",
+        opts = {
+            enabled = true,
+            message_template = " <summary> • <date> • <author> • <<sha>>",
+            date_format = "%m-%d-%Y %H:%M:%S",
+            virtual_text_column = 1,
+        },
+    }
 }
