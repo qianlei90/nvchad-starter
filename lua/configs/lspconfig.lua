ab870b9353e6f7c4b6ec976174de20ee9612b240
Qian Lei <qianlei90@gmail.com>
2024-12-11 10:51:26 +0800

    fix(lsp): fix config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,28 +1,26 @@
--- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
+local configs = require "nvchad.configs.lspconfig"
 
-local lspconfig = require "lspconfig"
-
-local servers = { "gopls", "golangci_lint_ls", "lua_ls" }
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
-lspconfig.lua_ls.setup {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = {
-          "vim",
+local servers = {
+  gopls = {},
+  golangci_lint_ls = {},
+  pylsp = {},
+  lua_ls = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = {
+            "vim",
+          }
         }
       }
     }
-  }
+  },
 }
+
+for name, opts in pairs(servers) do
+  opts.on_init = configs.on_init
+  opts.on_attach = configs.on_attach
+  opts.capabilities = configs.capabilities
+
+  require("lspconfig")[name].setup(opts)
+end
