77e7397a6d57b900d28d6fb023ca75359053c28d
qianlei.qianl <qianlei.qianl@bytedance.com>
2024-12-11 18:16:09 +0800

    fix(*): fix config

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,22 +1,23 @@
 require "nvchad.mappings"
 
 -- 切换 Tab
-vim.keymap.set('n', '<C-n>', ':tabnext<CR>', { desc = "Next Tab" })
-vim.keymap.set('n', '<C-p>', ':tabprev<CR>', { desc = "Previous Tab"})
+vim.keymap.set('n', '<C-t>', ':tabnew<CR>', { desc = "open new tab" })
+vim.keymap.set('n', '<C-n>', ':tabnext<CR>', { desc = "next tab" })
+vim.keymap.set('n', '<C-p>', ':tabprev<CR>', { desc = "previous tab"})
 -- 移动 Tab
 -- TODO: 未生效
 -- vim.keymap.set('n', '<S-Left>', '<Ecs>:tabmove -1<CR>', { desc = "Move Tab To Left" })
 -- vim.keymap.set('n', '<S-Right>', '<Ecs>:tabmove +1<CR>', { desc = "Move Tabl To Right"})
 
 -- 光标移动
-vim.keymap.set('n', 'H', '^', { desc = "Go To Head"})
-vim.keymap.set('v', 'H', '^', { desc = "Go To Head"})
-vim.keymap.set('n', 'L', '$', { desc = "Go To Tail"})
-vim.keymap.set('v', 'L', '$', { desc = "Go To Tail"})
-vim.keymap.set('n', 'J', 'L', { desc = "Go To Bottom"})
-vim.keymap.set('v', 'J', 'L', { desc = "Go To Bottom"})
-vim.keymap.set('n', 'K', 'H', { desc = "Go To Top"})
-vim.keymap.set('v', 'K', 'H', { desc = "Go To Top"})
+vim.keymap.set('n', 'H', '^', { desc = "go to head"})
+vim.keymap.set('v', 'H', '^', { desc = "go to head"})
+vim.keymap.set('n', 'L', '$', { desc = "go to tail"})
+vim.keymap.set('v', 'L', '$', { desc = "go to tail"})
+vim.keymap.set('n', 'J', 'L', { desc = "go to bottom"})
+vim.keymap.set('v', 'J', 'L', { desc = "go to bottom"})
+vim.keymap.set('n', 'K', 'H', { desc = "go to top"})
+vim.keymap.set('v', 'K', 'H', { desc = "go to top"})
 
 -- 命令模式时的移动
 vim.keymap.set('c', '<C-a>', '<Home>')
@@ -24,6 +25,9 @@ vim.keymap.set('c', '<C-e>', '<End>')
 vim.keymap.set('c', '<A-f>', '<S-Right>')
 vim.keymap.set('c', '<A-b>', '<S-Left>')
 
+-- 保存退出
 vim.keymap.set('i', '<C-s>', '<Ecs>:w<CR>')
-vim.keymap.set('n', '<C-t>', ':tabnew<CR>')
 vim.keymap.set('n', '<C-q>', ':q<CR>')
+
+-- gitblame
+vim.keymap.set('n', '<Leader>gb', ':GitBlameToggle<CR>', { desc = "toggle git blame"})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,7 +25,7 @@ return {
         "f-person/git-blame.nvim",
         event = "VeryLazy",
         opts = {
-            enabled = true,
+            enabled = false,
             message_template = " <summary> • <date> • <author> • <<sha>>",
             date_format = "%m-%d-%Y %H:%M:%S",
             virtual_text_column = 1,
