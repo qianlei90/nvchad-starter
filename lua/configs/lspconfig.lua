5afe9f3c406f43175f22e19c82647ce95d8a7e95
qianlei.qianl <qianlei.qianl@bytedance.com>
2025-02-19 21:09:58 +0800

    fix(lsp): disable harper_ls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,48 +1,48 @@
 local configs = require "nvchad.configs.lspconfig"
 
 local servers = {
-    autotools_ls = {},
-    bashls = {},
-    docker_compose_language_service = {},
-    dockerls = {},
-    golangci_lint_ls = {},
-    gopls = {},
-    harper_ls = {
-        settings = {
-            ["harper-ls"] = {
-                linters = {
-                    sentence_capitalization = false,
-                },
-            },
+  autotools_ls = {},
+  bashls = {},
+  docker_compose_language_service = {},
+  dockerls = {},
+  golangci_lint_ls = {},
+  gopls = {},
+  -- harper_ls = {
+  --     settings = {
+  --         ["harper-ls"] = {
+  --             linters = {
+  --                 sentence_capitalization = false,
+  --             },
+  --         },
+  --     },
+  -- },
+  helm_ls = {},
+  jsonls = {},
+  lua_ls = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = {
+            "vim",
+          },
         },
+      },
     },
-    helm_ls = {},
-    jsonls = {},
-    lua_ls = {
-        settings = {
-            Lua = {
-                diagnostics = {
-                    globals = {
-                        "vim",
-                    }
-                }
-            }
-        }
-    },
-    nginx_language_server = {},
-    nil_ls = {},
-    protols = {},
-    pylsp = {},
-    terraformls = {},
-    thriftls = {},
-    vimls = {},
-    yamlls = {},
+  },
+  nginx_language_server = {},
+  nil_ls = {},
+  protols = {},
+  pylsp = {},
+  terraformls = {},
+  thriftls = {},
+  vimls = {},
+  yamlls = {},
 }
 
 for name, opts in pairs(servers) do
-    opts.on_init = configs.on_init
-    opts.on_attach = configs.on_attach
-    opts.capabilities = configs.capabilities
+  opts.on_init = configs.on_init
+  opts.on_attach = configs.on_attach
+  opts.capabilities = configs.capabilities
 
-    require("lspconfig")[name].setup(opts)
+  require("lspconfig")[name].setup(opts)
 end
